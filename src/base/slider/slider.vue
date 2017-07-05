0248f24e11182698fc8b0c8f9bbf5dedd8bf93bc
LeventLei <443706199@qq.com>
2017-07-05 18:14:59 +0800

    slider.vue 50%

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot></slot>
+    </div>
+    <div class="dots"></div>
+  </div>
+</template>
+<script type='text/ecmascript-6'>
+	import BScroll from 'better-scroll'
+  export default {
+    props: {
+      loop: {
+        type: Boolean,
+        default: true
+      },
+      autoPlay: {
+        type: Boolean,
+        default: true
+      },
+      interval: {
+        type: Number,
+        default: 4000
+      }
+    },
+		mounted(){
+			setTimeout(() => {
+				this._setSliderWidth();
+				this._initSlider();
+			},20)
+		},
+		methods:{
+			_setSliderWidth(){
+				this.children = this.$refs.sliderGroup.children
+
+				let width = 0
+				let sliderWidth = this.$refs.slider.clientWidth
+				for (let i =0;i<this.children.length;i++){
+						let child = this.children[i]
+				}
+			},
+			_initSlider(){
+
+			}
+		},
+    data() {
+      return {}
+    }
+  }
+
+</script>
+
+
+<style lang='stylus' rel='stylesheet/stylus' scoped>
+	@import "~common/stylus/variable"
+
+	.slider
+		min-height :1px
+
+</style>
