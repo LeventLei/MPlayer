598cefdee24eb73c39e2eff6942dd8d0f6cc2bfc
LeventLei <443706199@qq.com>
2017-07-06 10:28:24 +0800

    slider.vue  80%

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -7,7 +7,10 @@
   </div>
 </template>
 <script type='text/ecmascript-6'>
-	import BScroll from 'better-scroll'
+  import BScroll from 'better-scroll'
+  import {
+    addClass
+  } from 'common/js/dom'
   export default {
     props: {
       loop: {
@@ -23,26 +26,44 @@
         default: 4000
       }
     },
-		mounted(){
-			setTimeout(() => {
-				this._setSliderWidth();
-				this._initSlider();
-			},20)
-		},
-		methods:{
-			_setSliderWidth(){
-				this.children = this.$refs.sliderGroup.children
+    mounted() {
+      setTimeout(() => {
+        this._setSliderWidth()
+        this._initSlider()
+      }, 20)
+    },
+    methods: {
+      _setSliderWidth() {
+        this.children = this.$refs.sliderGroup.children
 
-				let width = 0
-				let sliderWidth = this.$refs.slider.clientWidth
-				for (let i =0;i<this.children.length;i++){
-						let child = this.children[i]
-				}
-			},
-			_initSlider(){
-
-			}
-		},
+        let width = 0
+        let sliderWidth = this.$refs.slider.clientWidth
+        console.log(sliderWidth)
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i]
+          addClass(child, 'slider-item')
+          child.style.width = sliderWidth + 'px'
+          width += sliderWidth
+        }
+        if (this.loop) {
+          width += 2 * sliderWidth
+          console.log(width)
+        }
+        this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initSlider() {
+        this.slider = new BScroll(this.$refs.slider, {
+          scrollX: true,
+          scrollY: false,
+          momentum: false,
+          snap: true,
+          snapLoop: this.loop,
+          snapThreshold: 0.3,
+          snapSpeed: 400,
+          click: true
+        })
+      }
+    },
     data() {
       return {}
     }
@@ -51,10 +72,28 @@
 </script>
 
 
-<style lang='stylus' rel='stylesheet/stylus' scoped>
-	@import "~common/stylus/variable"
 
-	.slider
-		min-height :1px
 
-</style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .slider
+    min-height :1px
+    .slider-group
+      position :relative
+      overflow :hidden
+      white-space :nowrap
+      .slider-item
+        float :left
+        box-sizing :border-box
+        overflow :hidden
+        text-align :center
+        a
+          display :block
+          width :100%
+          overflow :hidden
+          text-decoration :none
+        img
+          display :block
+          width :100%
+</style>         
--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -11,4 +11,3 @@ export function hasClass(el, className) {
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
   return reg.test(el.className)
 }
-
