0e0a9accbcd26d5fc1b8d64dab22330e6c915b6d
LeventLei <443706199@qq.com>
2017-07-14 15:04:50 +0800

    slider.vue complete

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -3,7 +3,9 @@
     <div class="slider-group" ref="sliderGroup">
       <slot></slot>
     </div>
-    <div class="dots"></div>
+    <div class="dots">
+      <span class="dot" v-for="(item,index) in dots" :class="{active:currentPageIndex ===  index}" :key="index"></span>
+    </div>
   </div>
 </template>
 <script type='text/ecmascript-6'>
@@ -12,6 +14,12 @@
     addClass
   } from 'common/js/dom'
   export default {
+    data() {
+      return {
+        dots: [],
+        currentPageIndex: 0
+      }
+    },
     props: {
       loop: {
         type: Boolean,
@@ -29,13 +37,25 @@
     mounted() {
       setTimeout(() => {
         this._setSliderWidth()
+        this._initDots()
         this._initSlider()
+        if (this.autoPlay) {
+          this._play()
+        }
       }, 20)
+
+      window.addEventListener('resize', () => {
+        if (!this.slider) {
+          return
+        }
+        this._setSliderWidth(true)
+        this.slider.refresh()
+      })
     },
     methods: {
-      _setSliderWidth() {
+      _setSliderWidth(isResize) {
         this.children = this.$refs.sliderGroup.children
-
+        console.log(this.children.length)
         let width = 0
         let sliderWidth = this.$refs.slider.clientWidth
         console.log(sliderWidth)
@@ -45,11 +65,14 @@
           child.style.width = sliderWidth + 'px'
           width += sliderWidth
         }
-        if (this.loop) {
+        if (this.loop && !isResize) {
           width += 2 * sliderWidth
           console.log(width)
         }
         this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initDots() {
+        this.dots = new Array(this.children.length)
       },
       _initSlider() {
         this.slider = new BScroll(this.$refs.slider, {
@@ -59,18 +82,37 @@
           snap: true,
           snapLoop: this.loop,
           snapThreshold: 0.3,
-          snapSpeed: 400,
-          click: true
+          snapSpeed: 400
         })
+
+        this.slider.on('scrollEnd', () => {
+          let pageIndex = this.slider.getCurrentPage().pageX
+          if (this.loop) {
+            pageIndex -= 1
+          }
+          this.currentPageIndex = pageIndex
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+            this._play()
+          }
+        })
+      },
+      _play() {
+        let pageIndex = this.currentPageIndex + 1
+        if (this.loop) {
+          pageIndex += 1
+        }
+        this.timer = setTimeout(() => {
+          this.slider.goToPage(pageIndex, 0, 400)
+        }, this.interval)
       }
     },
-    data() {
-      return {}
+    destoryed() {
+      clearTimeout(this.timer)
     }
   }
 
 </script>
-
 
 
 
@@ -96,4 +138,22 @@
         img
           display :block
           width :100%
+    .dots
+      position :absolute
+      right :0
+      left :0
+      bottom :12px
+      text-align :center
+      font-size :0
+      .dot
+        display :inline-block
+        margin :0 4px
+        width :8px
+        height :8px
+        border-radius :50%
+        background :$color-text-l
+        &.active
+          width :20px
+          border-radius :5px
+          background :$color-text-ll          
 </style>         
